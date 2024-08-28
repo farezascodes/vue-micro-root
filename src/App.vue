e3667b7d2867b78af91138e14a94bd6703442fd2
Farezas <fareza087@gmail.com>
2024-08-28 15:40:28 +0700

    first init
    project demo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+
+<nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</header>
+
+<RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <main>
+
+    <FormCoba></FormCoba>
+  </main>
+</template>
+<script>
+import FormCoba from '@/components/FormCoba.vue';
+export default {
+  components: { FormCoba }
+}
+</script>
